0f2094001bbdc2601e42ae55f8057aaee16a226b
Alan <56948680+waterrmalann@users.noreply.github.com>
2024-02-08 20:25:04 +0530

    feat(zod): add /signup post schemas

---
+++ src/schemas/users/postSignupSchemas.ts
@@ -0,0 +1,24 @@
+// POST /signup
+
+import { Request } from "express";
+import { z } from "zod";
+
+/** Validates `req.body` */
+const bodySchema = z.object({
+    name: z.string()
+        .min(3),
+
+    email: z.string()
+        .email(),
+
+    password: z.string()
+        .min(8)
+        .regex(/^[a-zA-Z0-9]{8,30}$/) // Alphanumeric, 8-30 characters
+});
+
+type PostSignupBodyType = z.infer<typeof bodySchema>;
+
+export { bodySchema as postSignupSchema };
+export type { PostSignupBodyType };
+
+export type PostSignupRequest = Request<any, any, PostSignupBodyType, any>;
