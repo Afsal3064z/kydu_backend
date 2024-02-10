045105b1bcf0cbddadab4f4de273eca304074112
Alan <56948680+waterrmalann@users.noreply.github.com>
2024-02-10 11:52:17 +0530

    feat: make logging more verbose

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import { Router as expressRouter } from 'express';
+import morgan from 'morgan';
 
 import authRouter from './routers/authRouter.js';
 import gigRouter from './routers/gigRouter.js';
@@ -19,13 +20,17 @@ mongoose.connect(process.env.MONGO_URI as string)
     });
 
 const app = express();
+app.disable('x-powered-by');
+if (process.env.NODE_ENV as string === 'development') {
+    app.use(morgan('dev'))
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', async (_req: Request, res: Response) => {
-    res.send({ status: "API is online." });
+    res.status(200).send({ status: "API is online." });
 });
 
 app.use("/api/", authRouter as expressRouter);
@@ -37,9 +42,15 @@ app.get('*', (_req: Request, res: Response) => res.sendStatus(404));
 app.use((err: Error, req: Request, res: Response, next: NextFunction): void => {
     console.error(err.stack)
     res.status(500).json({ error: "An internal server error has occured. "});
-  })
+})
 
 const PORT: number = parseInt(process.env.PORT as string, 10) || 3000;
 app.listen(PORT, () => {
+    console.log(" ██╗  ██╗██╗   ██╗██████╗ ██╗   ██╗");
+    console.log(" ██║ ██╔╝╚██╗ ██╔╝██╔══██╗██║   ██║");
+    console.log(" █████╔╝  ╚████╔╝ ██║  ██║██║   ██║");
+    console.log(" ██╔═██╗   ╚██╔╝  ██║  ██║██║   ██║");
+    console.log(" ██║  ██╗   ██║   ██████╔╝╚██████╔╝");
+    console.log(" ╚═╝  ╚═╝   ╚═╝   ╚═════╝  ╚═════╝ ");
     console.log(`[server] ⚡ Server has started listening on http://localhost:${PORT}`);
 });
